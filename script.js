37f8c4876a5717c0f1b62153ca41c3639d538e90
Rahul Patel <rahulpatelue188076@gmail.com>
2024-05-20 10:13:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navToggle = document.getElementById('nav-toggle');
-    const navLinks = document.getElementById('nav-links');
+// JavaScript for Scroll Animation
 
-    navToggle.addEventListener('click', function() {
-        navLinks.classList.toggle('active');
-    });
+document.addEventListener('DOMContentLoaded', () => {
+    const scrollElements = document.querySelectorAll('.scrolly');
 
-    // Smooth scrolling for anchor links
-    const links = document.querySelectorAll('nav ul li a');
-    for (const link of links) {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-            const targetId = link.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
+    const elementInView = (el, dividend = 1) => {
+        const elementTop = el.getBoundingClientRect().top;
 
-            if (targetElement) {
-                targetElement.scrollIntoView({ behavior: 'smooth' });
-                navLinks.classList.remove('active'); // Close nav menu on link click (mobile)
+        return (
+            elementTop <= (window.innerHeight || document.documentElement.clientHeight) / dividend
+        );
+    };
+
+    const elementOutofView = (el) => {
+        const elementTop = el.getBoundingClientRect().top;
+
+        return (
+            elementTop > (window.innerHeight || document.documentElement.clientHeight)
+        );
+    };
+
+    const displayScrollElement = (element) => {
+        element.classList.add('scrolled');
+    };
+
+    const hideScrollElement = (element) => {
+        element.classList.remove('scrolled');
+    };
+
+    const handleScrollAnimation = () => {
+        scrollElements.forEach((el) => {
+            if (elementInView(el, 1.25)) {
+                displayScrollElement(el);
+            } else if (elementOutofView(el)) {
+                hideScrollElement(el);
             }
-        });
+        })
     }
+
+    window.addEventListener('scroll', () => {
+        handleScrollAnimation();
+    });
 });
